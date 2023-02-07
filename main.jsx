e9134bc6d6e22effbfd9016014dc3475ca0d1610
yamolatix <yamilaortiztoscano@gmail.com>
2023-02-07 07:35:59 -0300

    punto de entrada a react

--- main.jsx
+++ main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client"
+import { App } from "./src/App"
 
 const root = createRoot(document.getElementById("app"))
 
-root.render(<h1>Hello World!</h1>)
+root.render(<App />)
